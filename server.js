06ad6f14ce8716fe7468c66aff793f3f2c93d81e
Spencer Markin <markinspencer@gmail.com>
2019-01-14 06:40:26 -0500

    add schema to server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
 
 const port = 4000;
 
 const app = express();
-app.use("/graphql", expressGraphQL({ graphiql: true }));
+app.use("/graphql", expressGraphQL({ schema, graphiql: true }));
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
