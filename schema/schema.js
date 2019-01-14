c1391f7fde578196d752be7b01e38b19558571ea
Spencer Markin <markinspencer@gmail.com>
2019-01-14 18:44:39 -0500

    add company type and associate it with users

--- schema/schema.js
+++ schema/schema.js
@@ -3,12 +3,30 @@ const axios = require("axios");
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLInt }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        const { companyId } = parentValue;
+        return axios
+          .get(`http://localhost:3000/companies/${companyId}`)
+          .then(resp => resp.data);
+      }
+    }
   }
 });
 
@@ -22,6 +40,16 @@ const RootQuery = new GraphQLObjectType({
         const { id } = args;
         return axios
           .get(`http://localhost:3000/users/${id}`)
+          .then(resp => resp.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        const { id } = args;
+        return axios
+          .get(`http://localhost:3000/companies/${id}`)
           .then(resp => resp.data);
       }
     }
