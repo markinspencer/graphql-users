0a2277fa28dfab8308efa2a35a71b20cccee7dbc
Spencer Markin <markinspencer@gmail.com>
2019-01-14 19:01:44 -0500

    Bidirectional realations with company and user

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,35 @@
 const graphql = require("graphql");
 const axios = require("axios");
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList
+} = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLInt }
-  }
+    description: { type: GraphQLInt },
+    users: {
+      type: GraphQLList(UserType),
+      resolve(parentValue, args) {
+        const { id } = parentValue;
+        return axios
+          .get(`http://localhost:3000/companies/${id}/users`)
+          .then(resp => resp.data);
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -27,7 +42,7 @@ const UserType = new GraphQLObjectType({
           .then(resp => resp.data);
       }
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
